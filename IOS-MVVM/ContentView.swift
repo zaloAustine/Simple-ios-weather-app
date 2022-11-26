209abbc70891cd3752a1647451f7294b36a0d01c
austineZalo <austinezaloa@gmail.com>
2022-11-26 08:14:48 +0300

    Initial Commit

---
+++ IOS-MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS-MVVM
+//
+//  Created by Zalo Austine on 26/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOS-MVVM/IOS_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_MVVMApp.swift
+//  IOS-MVVM
+//
+//  Created by Zalo Austine on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct IOS_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
